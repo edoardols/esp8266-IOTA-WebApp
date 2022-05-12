5db02006d35c43c2b54f8d0fa868023dd078af43
Edoardo Lascala <100836351+edoardols@users.noreply.github.com>
2022-05-12 22:29:32 +0200

    Update message.js

--- server/message.js
+++ server/message.js
@@ -1,9 +1,8 @@
-let index_iota = 'TEST_WEB_APP4';
+let index_iota = 'TEST_WEB_APP5';
 
-
-// TEMPORANEI
 let messageIdsRead = [];
 let databaseArray = [];
+
 
 function getDatabaseArray() {
     return databaseArray;
@@ -15,54 +14,45 @@ function searchMessage() {
 
 function updateMessageIdsArray() {
 
+    //INDEX for data
+    let DATA_INDEX = 0;
+    let TIME_INDEX = 1;
+    let TEMPERATURE_INDEX = 2;
+    let HUMIDITY_INDEX = 3;
+    let CO2PPM_INDEX = 4;
+
     fetch('https://tangle.iotaqubic.us/api/v1/messages?index=' + ascii2hex(index_iota))
     .then(result => result.json())
     .then((messageMatchingIndex) => {
 
-        //console.log('Message Matching Index: ', messageMatchingIndex);
-
-        // messageIds array for that index_iota
-        //output.data.messageIds
         let messageIdsArray = messageMatchingIndex.data.messageIds;
-        //console.log(messageIdsArray);
 
         for(let i = 0; i < messageIdsArray.length ; i++) {
-            //Array.isArray(variableName) check if that messageIds is already in messageIdsRead
+            
             if( messageIdsRead.indexOf(messageIdsArray[i]) == -1){
+                
                 messageIdsRead.push(messageIdsArray[i]);
-                //console.log('Added: ', messageIdsArray[i]);
-
-                //make a HTTPS request given that messageIds
+                
                 fetch('https://tangle.iotaqubic.us/api/v1/messages/' + messageIdsArray[i])
                 .then(result => result.json())
                 .then((messages) => {
 
                     let messageJSONFormat = JSON.parse(hex2ascii(messages.data.payload.data));
-                    //console.log('Message: ', messageJSONFormat);
 
-                    let newArray = [messageMatchingIndex.data.messageIds[i], messageJSONFormat.data.sensorData.day, messageJSONFormat.data.sensorData.time, messageJSONFormat.data.sensorData.temperature, messageJSONFormat.data.sensorData.humidity ];
+                    let messageToArray = [];
+                    messageToArray[DATA_INDEX ] = messageJSONFormat.data.sensorData.day;
+                    messageToArray[TIME_INDEX] = messageJSONFormat.data.sensorData.time;
+                    messageToArray[TEMPERATURE_INDEX] = messageJSONFormat.data.sensorData.temperature;
+                    messageToArray[HUMIDITY_INDEX] = messageJSONFormat.data.sensorData.humidity;
+                    messageToArray[CO2PPM_INDEX] = messageJSONFormat.data.sensorData.co2_ppm;
 
-                    //console.log(newArray);
-
-                    //ONLY SENSOR DATA FOR TEST
-                    addToDatabase(newArray);
+                    databaseArray.push(messageToArray);
+                    drawChart();
 
                 }).catch(err => console.error(err));
             }
         }
 
-        //console.log('MessageIds Array:', messageIdsRead);
-
-        //console.log('Database Array:', databaseArray);
-
-        //console.log("DEBUG: end fetch");
-
     }).catch(err => console.error(err));
 
 }
-
-function addToDatabase(arrayToPush) {
-    databaseArray.push(arrayToPush);
-}
-
-
